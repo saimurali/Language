1f870c310d7e9e3ecf724777563a1bcd5cb6705e
saimurali <sai_nda@rediffmail.com>
2020-09-11 12:38:44 +0530

    Add project files.

---
+++ Language/CreateCul.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+public static class CreateCul
+{
+    public static void Create()
+    {
+        // Create a custom culture for ru-US.
+        CultureAndRegionInfoBuilder culAndReg = new CultureAndRegionInfoBuilder("en-HU",
+                                               CultureAndRegionModifiers.None);
+        culAndReg.LoadDataFromCultureInfo(CultureInfo.CreateSpecificCulture("en-HU"));
+        culAndReg.LoadDataFromRegionInfo(new RegionInfo("HU"));
+
+        culAndReg.CultureEnglishName = "english (Hungary)";
+        culAndReg.CultureNativeName = "english (Hungary)";
+        culAndReg.CurrencyNativeName = "Hungary INR";
+        culAndReg.RegionNativeName = "Hungarian-English";
+
+        // Register the culture.
+        try
+        {
+            culAndReg.Register();
+            Console.Write("The new Culture and region is created for {0} :" + "en-HU");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.Write("The Operation Failed due to " + ex.Message);
+        }   
+
+       
+    }
+}
---
+++ Language/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Language
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CULTURE ISO ISO WIN DISPLAYNAME                              ENGLISHNAME");
+
+            CreateCul.Create();
+          
+            var test = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                Console.Write("{0,-7}", ci.Name);
+                Console.Write(" {0,-3}", ci.TwoLetterISOLanguageName);
+                Console.Write(" {0,-3}", ci.ThreeLetterISOLanguageName);
+                Console.Write(" {0,-3}", ci.ThreeLetterWindowsLanguageName);
+                Console.Write(" {0,-40}", ci.DisplayName);
+                Console.WriteLine(" {0,-40}", ci.EnglishName);
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
